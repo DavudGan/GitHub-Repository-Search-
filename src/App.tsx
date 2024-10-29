70889c161e2b0da0ee0844f9e0f4f9c4b1f3d475
Davud <davud@MacBook-Pro-Davud.local>
2024-10-29 15:30:11 +0500

    feat: реализован UI и mobx стор с запросом данных

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useCallback } from "react";
+import { observer } from "mobx-react-lite";
+import repositoryStore from "./Store/data_store";
+import { Repository } from "./Store/data_store";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CreateIcon from "@mui/icons-material/Create";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  CircularProgress,
+  IconButton,
+} from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const RepositoryList = observer(() => {
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    repositoryStore.fetchRepositories("react");
+  }, [repositoryStore.fetchRepositories]);
+
+  const lastRepositoryRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (repositoryStore.loading) return;
+      if (observerRef.current) observerRef.current.disconnect();
+
+      observerRef.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && repositoryStore.hasNextPage) {
+          repositoryStore.fetchRepositories("react");
+        }
+      });
+
+      if (node) observerRef.current.observe(node);
+    },
+    [
+      repositoryStore.loading,
+      repositoryStore.fetchRepositories,
+      repositoryStore.hasNextPage,
+    ]
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Box sx={{ maxWidth: 800, margin: "auto", padding: 2 }}>
+      <Typography variant="h4" gutterBottom>
+        Repositories
+      </Typography>
+      <Box>
+        {repositoryStore.repositories.map((repo: Repository, index: number) => (
+          <Card
+            key={repo.id}
+            ref={
+              index === repositoryStore.repositories.length - 10
+                ? lastRepositoryRef
+                : null
+            }
+            sx={{
+              marginBottom: 2,
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <CardContent>
+              <Typography variant="h6">{repo.name}</Typography>
+              <Typography variant="body2" color="text.secondary">
+                {repo.description || "No description available"}
+              </Typography>
+              <Typography variant="body2" sx={{ fontWeight: "bold" }}>
+              ✰: {repo.stargazerCount}
+              </Typography>
+            </CardContent>
+            <Box display="flex">
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                sx={{ marginRight: 2 }}
+                onClick={()=>console.log('Bay')}
+              >
+                <DeleteIcon />
+              </IconButton>
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                sx={{ marginRight: 2 }}
+              >
+                <CreateIcon />
+              </IconButton>
+            </Box>
+          </Card>
+        ))}
+      </Box>
+      {repositoryStore.loading && (
+        <Box display="flex" justifyContent="center" marginY={2}>
+          <CircularProgress />
+        </Box>
+      )}
+    </Box>
+  );
+});
 
-export default App
+export default RepositoryList;
---
+++ src/Store/data_store.ts
@@ -0,0 +1,122 @@
+import { makeAutoObservable, runInAction } from "mobx";
+
+const GITHUB_API_URL = "https://api.github.com/graphql";
+const GITHUB_TOKEN = import.meta.env.VITE_REACT_APP_GITHUB_TOKEN;
+
+interface LicenseInfo {
+  name: string | null;
+}
+
+interface PrimaryLanguage {
+  name: string | null;
+}
+
+export interface Repository {
+  id: string;
+  name: string;
+  stargazerCount: number;
+  updatedAt: string;
+  description: string | null;
+  primaryLanguage: PrimaryLanguage | null;
+  licenseInfo: LicenseInfo | null;
+}
+
+interface Edge {
+  node: Repository;
+}
+
+interface PageInfo {
+  endCursor: string | null;
+  hasNextPage: boolean;
+}
+
+interface GitHubAPIResponse {
+  data: {
+    search: {
+      edges: Edge[];
+      pageInfo: PageInfo;
+    };
+  };
+}
+
+class RepositoryStore {
+  displayedRepositories: Repository[] = [];
+  repositories: Repository[] = [];
+  loading = false;
+  error: string | null = null;
+  endCursor: string | null = null;
+  hasNextPage = true;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  async fetchRepositories(searchTerm: string) {
+    if (this.loading || !this.hasNextPage) return;
+    this.loading = true;
+    this.error = null;
+
+    const query = `
+      query ($searchTerm: String!, $after: String) {
+        search(query: $searchTerm, type: REPOSITORY, first: 20, after: $after) {
+          edges {
+            node {
+              ... on Repository {
+                id
+                name
+                stargazerCount
+                updatedAt
+                description
+                primaryLanguage {
+                  name
+                }
+                licenseInfo {
+                  name
+                }
+              }
+            }
+          }
+          pageInfo {
+            endCursor
+            hasNextPage
+          }
+        }
+      }
+    `;
+
+    try {
+      const response = await fetch(GITHUB_API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${GITHUB_TOKEN}`,
+        },
+        body: JSON.stringify({
+          query,
+          variables: { searchTerm, after: this.endCursor },
+        }),
+      });
+
+      const result: GitHubAPIResponse = await response.json();
+      const { edges, pageInfo } = result.data.search;
+
+      runInAction(() => {
+        this.repositories.push(...edges.map((edge: Edge) => edge.node));
+        this.endCursor = pageInfo.endCursor;
+        this.hasNextPage = pageInfo.hasNextPage;
+      });
+      console.log("ds");
+    } catch (error: any) {
+      runInAction(() => {
+        this.error = error.message;
+      });
+    } finally {
+      runInAction(() => {
+        this.loading = false;
+      });
+    }
+  }
+}
+
+const repositoryStore = new RepositoryStore();
+export default repositoryStore;
